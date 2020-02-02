ada25778f3e1ebbb2c96cad75a1d5962f080cd69
Iryna Manukhina <iriny.x@gmail.com>
2020-02-01 19:49:08 -0800

    Added test cases for sum function

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const {sum, mult} = require('../index.js');
 
 describe('function sum', () => {
+
     it('should func sum equals 6', () => {
         assert.equal(sum(2, 4), 6);
     });
@@ -11,13 +12,25 @@ describe('function sum', () => {
 
     });
 
+    it('should func sum equals 55', () => {
+        assert.equal(sum(5, '5'), 55);
+    });
+
+    it('should func sum equals s5', () => {
+        assert.notEqual(sum('s', 5), 5);
+    })
+
 });
 
+describe('function mult', () => {
 
-it('should func mult a equals 8', () => {
-    assert.equal(mult(2, 4), 8);
+    it('should func mult a equals 8', () => {
+        assert.equal(mult(2, 4), 8);
+    });
+
+    it('should func mult not equals 0', () => {
+        assert.notEqual(mult(2, 4), 0);
+    });
+
 });
 
-it('should func mult not equals 0', () => {
-    assert.notEqual(mult(2, 4), 0);
-});
